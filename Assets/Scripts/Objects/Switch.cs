5f1ad129c4c40d6350ad1a5bcab97135453fbaf2
Daniel Stout <dstout55@gmail.com>
2020-04-16 20:42:08 -0300

    Added switched dungeon door

---
+++ Assets/Scripts/Objects/Switch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public BoolValue pressed;
+
+    public Sprite pressedSprite;
+
+    public GameObject door;
+
+    private SpriteRenderer spriteRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+
+        if (pressed.runtimeValue)
+        {
+            door.SetActive(false);
+            PressSwitch();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            pressed.runtimeValue = true;
+            PressSwitch();
+        }
+    }
+
+    private void PressSwitch()
+    {
+        door.SetActive(false);
+        spriteRend.sprite = pressedSprite;
+    }
+}
